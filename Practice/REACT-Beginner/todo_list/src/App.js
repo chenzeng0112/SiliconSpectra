dce5728cb3937d1f29892ca831d42d8f2c4d5681
Zeng Chen <chenzeng0112@gmail.com>
2020-04-14 00:33:28 -0500

    no message

--- Practice/REACT-Beginner/todo_list/src/App.js
+++ Practice/REACT-Beginner/todo_list/src/App.js
@@ -1,15 +1,28 @@
 import React from "react"
 import TodoItem from "./TodoItem"
 import todosData from "./todosData"
+import "./index.css"
 
-function App() {
-    const todoItem = todosData.map(item => <TodoItem key={item.id} item={item} />)
+// function App() {
+//     const todoItem = todosData.map(item => <TodoItem key={item.id} item={item} />)
 
-    return (
-        <div className="todo-list">
-            {todoItem}
-        </div>
-    )
+//         return (
+//             <div className="todo-list">
+//                 {todoItem}
+//             </div>
+//         )
+// }
+
+class App extends React.Component {
+    render() {
+        const todoItem = todosData.map(item => <TodoItem key={item.id} item={item} />)
+
+        return (
+            <div className="todo-list">
+                {todoItem}
+            </div>
+        )
+    }
 }
 
 export default App

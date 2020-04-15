223691222f778796261de9f3a1fae1016648d3c3
Zeng Chen <chenzeng0112@gmail.com>
2020-04-15 08:01:02 -0500

    no message

--- Practice/REACT-Beginner/todo_list/src/App.js
+++ Practice/REACT-Beginner/todo_list/src/App.js
@@ -3,21 +3,31 @@ import TodoItem from "./TodoItem"
 import todosData from "./todosData"
 import "./index.css"
 
-// function App() {
-//     const todoItem = todosData.map(item => <TodoItem key={item.id} item={item} />)
-
-//         return (
-//             <div className="todo-list">
-//                 {todoItem}
-//             </div>
-//         )
-// }
-
 class App extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            todos: todosData
+        }
+    }
 
+    handleChange = (id) => {
+        this.setState(prevState => {
+            const updatedTodos = prevState.todos.map(todo => {
+                if (todo.id === id) {
+                    todo.completed = !todo.completed
+                }
+                return todo
+            })
+            return {
+                todos: updatedTodos
+            }
+        })
+    }
 
     render() {
-        const todoItem = todosData.map(item => <TodoItem key={item.id} item={item} />)
+        const todoItem = this.state.todos.map(item => <TodoItem key={item.id} item={item}
+            handleChange={this.handleChange} />)
 
         return (
             <div className="todo-list">

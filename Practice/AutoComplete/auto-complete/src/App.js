f374df5a9e91687dcfc57ad27662ae756e453f88
Zeng Chen <chenzeng0112@gmail.com>
2020-04-24 10:31:57 -0500

    no message

--- Practice/AutoComplete/auto-complete/src/App.js
+++ Practice/AutoComplete/auto-complete/src/App.js
@@ -1,16 +1,68 @@
 import React from 'react';
+import axios from 'axios';
 import './App.css';
 
 class App extends React.Component {
+  state = {
+    suggestions: [],
+    searchText: '',
+    showResult: false,
+  }
+
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/comments')
+      .then(res => this.setState({ suggestions: res.data }));
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      searchText: event.target.value,
+      showResult: true,
+    });
+  }
+
+  handleClick = (event) => {
+    this.setState({
+      showResult: false,
+      // suggestions: [],
+      searchText: event.target.innerText,
+    })
+  }
+
   render() {
+    const { suggestions, searchText, showResult } = this.state;
+    let displayResult = suggestions.filter(suggestion =>
+      suggestion.name.toLowerCase().includes(searchText.toLowerCase())
+    );
+
+    let displayUpdate = [];
+    if (showResult !== false && searchText !== '') {
+      displayUpdate = (
+        <ul className='output'>
+          {displayResult.map(item =>
+            <li
+              key={item.id}
+              onClick={this.handleClick}
+            >
+              {item.name}
+            </li>)}
+        </ul>
+      )
+    }
+
     return (
-      <div className='inputBox'>
-        <input
-          tpye='text'
-          placeholder='AUTO COMPLETE ...'
-          className='inputText'
-        />
-      </div>
+      <React.Fragment>
+        <div className='inputBox'>
+          <input
+            tpye='text'
+            value={searchText}
+            placeholder='AUTO COMPLETE ...'
+            className='inputText'
+            onChange={this.handleChange}
+          />
+        </div>
+        {displayUpdate}
+      </React.Fragment>
     )
   }
 }

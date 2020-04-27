38b04e6ead58f1039e786db27f5d673c63923127
Zeng Chen <chenzeng0112@gmail.com>
2020-04-27 09:18:32 -0500

    no message

--- HW_3/todo_list/src/App.js
+++ HW_3/todo_list/src/App.js
@@ -3,8 +3,18 @@ import './App.css';
 import { v4 as uuid } from 'uuid';
 
 import Header from './Components/Header.js';
+import AddTodo from './Components/AddTodo.js';
 
 class App extends React.Component {
+  state = {
+    todos: [],
+  }
+
+  // addTodo
+  addTodo = () => {
+
+  }
+
   render() {
     return (
       <div>
---
+++ HW_3/todo_list/src/Components/AddTodo.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class AddTodo extends React.Component {
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
+
+export default AddTodo

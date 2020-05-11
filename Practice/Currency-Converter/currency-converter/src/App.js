2915dec35406eefdf2e516ac7b5d00f22c7b5951
Zeng Chen <chenzeng0112@gmail.com>
2020-05-11 09:58:52 -0500

    initial

---
+++ Practice/Currency-Converter/currency-converter/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './App.css';
+import Converter from './Components/Converter.js'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Converter />
+      </div>
+    )
+  }
+}
+
